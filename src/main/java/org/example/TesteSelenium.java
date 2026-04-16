a6f847cedf6ed6d3394ad66afafe60f1bed9fc78
PedrooAmaral <pedro.amaral@sptech.school>
2026-04-16 14:59:06 -0300

    Feat: Teste de campos escritos

---
+++ src/main/java/org/example/TesteSelenium.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class TesteSelenium {
+    public static void main(String[] args){
+        //configurando o ChromeDriver automaticamente
+        WebDriverManager.chromedriver().setup();
+        //criando instancia do chrome
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //abrindo o chrome
+        driver.get("https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=HBQ2z9fdp0WwcxEfZtCzDFimoWho9WhOmnQCBjqY305UMU1GMEU1UlBRVEFYSUZNRkIyUzdKQlA2TS4u");
+
+        //localizando a barra de busca
+        WebElement campoResposta = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("textarea[data-automation-id='textInput']")
+        ));
+        //digitando o texto na barra de busca
+        campoResposta.sendKeys("selenium on");
+
+        WebElement botaoEnviar = driver.findElement(By.cssSelector("button[data-automation-id='submitButton']"));
+        //enviando a resposta
+        botaoEnviar.click();
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("div[data-automation-id='thankYouMessage']")
+        ));
+
+        //driver.get("https://dev.to/jhonifaber/hexagonal-architecture-or-port-adapters-23ed?utm_source=chatgpt.com");
+        //mostrando o titulo da página no console
+        //System.out.println("titulo:" + driver.getTitle());
+        //fechando o navegador
+        driver.quit();
+    }
+
+}
