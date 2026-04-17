2dac978f719f55135297c3868241505631e4d5ec
PedrooAmaral <pedro.amaral@sptech.school>
2026-04-17 11:14:08 -0300

    feat(form): automate choice and multiple answers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Selenium-Teste
 Automação de Microsoft Forms usando Selenium WebDriver em Java, com foco em waits explícitos, seletores estáveis e fluxo completo de preenchimento e envio de respostas.
+
--- src/main/java/org/example/TesteSelenium.java
+++ src/main/java/org/example/TesteSelenium.java
@@ -27,19 +27,31 @@ public class TesteSelenium {
         //digitando o texto na barra de busca
         campoResposta.sendKeys("selenium on");
 
+        //achando span de click
+        WebElement choice = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("span[data-automation-id='radio'][data-automation-value='sim']")
+        ));
+        choice.click();
+
+        //span de click de multipla escolha
+        WebElement multiChoice = wait.until(ExpectedConditions.elementToBeClickable(
+               By.cssSelector("span[data-automation-id='checkbox'][data-automation-value='perfeitamente']")
+        ));
+        multiChoice.click();
+
+        // achando botao
         WebElement botaoEnviar = driver.findElement(By.cssSelector("button[data-automation-id='submitButton']"));
         //enviando a resposta
         botaoEnviar.click();
-
+        //esperando msg de confirmação
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
                 By.cssSelector("div[data-automation-id='thankYouMessage']")
         ));
-
         //driver.get("https://dev.to/jhonifaber/hexagonal-architecture-or-port-adapters-23ed?utm_source=chatgpt.com");
         //mostrando o titulo da página no console
         //System.out.println("titulo:" + driver.getTitle());
         //fechando o navegador
-        driver.quit();
+        //driver.quit();
     }
 
 }
